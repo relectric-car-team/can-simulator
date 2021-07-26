bd2fd412a1e25833d6f4cd6598b06f1a2bde4499
Robert Brown <brown.rjb75@gmail.com>
2021-07-25 21:53:14 -0600

    feat: add serial communication :sparkles:

--- can_simulator/simulator.py
+++ can_simulator/simulator.py
@@ -1,3 +1,9 @@
-if __name__ == "__main__":
-    print("nothing happening yet 😞💔"
+import serial
 
+if __name__ == "__main__":
+    print("nothing happening yet 😞💔")
+    PORT = "COM2" # example port
+    ser = serial.Serial(port = PORT, baudrate=115200, bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
+    while(1):
+        ser.write(b'Hello World') # writes hello world to the port
+    ser.close() # closes the port
